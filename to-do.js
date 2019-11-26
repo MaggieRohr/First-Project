9b09de907ae1b08ce3b304d56c5704cf1c55e4a9
Maggie <58152995+MaggieRohr@users.noreply.github.com>
2019-11-26 16:02:28 -0600

    CSS styling
    Adding more css styling. Including shadows, opacitys, and an add button.

---
+++ to-do.js
@@ -0,0 +1,26 @@
+//Creating a new to-do when clicking on the add button
+
+function newElements() {
+  var li = document.createElement("li");
+  var inputTodo = document.getElementsByClassName("addBtn").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+}
