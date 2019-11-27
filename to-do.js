a56faba5f4d7e0dca66358abd61d02cee01bf725
Maggie <58152995+MaggieRohr@users.noreply.github.com>
2019-11-26 20:34:05 -0600

    css styling
    Played around with more css styling for the delete buttons

--- to-do.js
+++ to-do.js
@@ -1,20 +1,20 @@
-//Creating a new to-do when clicking on the add button
+var input = document.querySelector("input[type = 'text']");
+var ul = document.querySelector("ul");
+var lists = document.querySelectorAll("li");
+var spans = document.getElementsByTagName("span");
+
+//Creating a new to-do when clicking the enter button
+
+
+//Creating a new todo when clicking the add button
+
+
 
 
 //Click on the trash can icons to close out the items entered
-var delete = document.getElementsByClassName("material-icons");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+
 
 //Creating a strike-through when clicking on an list item elements
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'li') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+
+
+//Add a strike-through if list item is clicked
