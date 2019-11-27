9a6eef718d5128d6740feba468daed76e52bd9eb
Maggie <58152995+MaggieRohr@users.noreply.github.com>
2019-11-26 18:07:56 -0600

    Rechanged the name of my to-do list file
    Changed the name to index.html

--- to-do.js
+++ to-do.js
@@ -1,26 +1,20 @@
 //Creating a new to-do when clicking on the add button
 
-function newElements() {
-  var li = document.createElement("li");
-  var inputTodo = document.getElementsByClassName("addBtn").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
+
+//Click on the trash can icons to close out the items entered
+var delete = document.getElementsByClassName("material-icons");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
   }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
 }
+
+//Creating a strike-through when clicking on an list item elements
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'li') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
