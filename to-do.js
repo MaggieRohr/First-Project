9de12d03694cb56ec1fcaf5e94ff79052fff92c7
Maggie <58152995+MaggieRohr@users.noreply.github.com>
2019-11-30 10:58:26 -0600

    Update to-do.js
    Added js code

--- to-do.js
+++ to-do.js
@@ -1,15 +1,53 @@
-//Creating a new to-do when clicking the enter button
+
+
+
+
+
+class ToDo{
+	constructor(value, id){
+		this.mVal = value;
+		this.mId = id;
+	}
+}
+
+var todos = []
+var counter = 0;
 var ul = document.querySelector("ul");
 document.querySelector('#newInput').addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-      var li = document.createElement("li");
-      var spanElement = document.createElement("span");
-      var icon = document.createElement("i");
-
-      var newTodoListItem = this.value;
-      this.value = " " ;
-      ul.appendChild(li).append(spanElement,newTodoListItem);
+    	var newTodo = new ToDo(this.value, counter);
+    	todos.push(newTodo);
+    	counter++;
+    	render();
+	  	this.value = " " ;
     }
 });
 
-//Click on the trash can icons to close out the items entered
+
+function render(){
+	ul.innerHTML = "";
+	for (i in todos){
+
+	  	var li = document.createElement("li");
+
+	  	var spanElement = document.createElement("span");
+
+	  	var newTodoListItem = todos[i].mVal;
+
+	  	spanElement.innerHTML = `<i id="${todos[i].mId}" onclick="del(${todos[i].mId})" class="material-icons">delete</i>`;
+
+	  	ul.appendChild(li).append(spanElement,newTodoListItem);
+	}
+
+
+};
+
+
+function del(id){
+	for (i in todos){
+		if(todos[i].mId == id){
+			todos.splice(i, 1);
+		}
+	}
+	render();
+}
